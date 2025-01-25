0bd39b72a6ab8cc3a9bedde24b4264e55f76a350
Jami <ahmedsyedmasrur@gmail.com>
2025-01-25 04:14:26 -0500

    Readme, Demo video and Build

---
+++ Project/Assets/Scripts/CubeList.cs
@@ -0,0 +1,18 @@
+/*
+Script for toggling the cube list
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CubeList : MonoBehaviour{
+    public GameObject Panel;
+    public List<GameObject> Cubes = new List<GameObject>();
+    
+    public void onTogglePanel(){
+        bool isActive = Panel.activeSelf;
+        Panel.SetActive(!isActive);
+    }
+}
